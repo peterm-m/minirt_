70cbc53c228e09c2b075b0714ac47cd4fbd8ff94
pedromar <pedromar@student.42madrid.com>
2024-07-03 21:28:13 +0200

    add parser to main

--- include/minirt.h
+++ include/minirt.h
@@ -6,7 +6,7 @@
 /*   By: pedromar <pedromar@student.42madrid.com                              */
 /*                                                                            */
 /*   Created: 2024/05/11 17:45:23 by pedromar                                 */
-/*   Updated: 2024/07/03 14:01:04 by pedromar                                 */
+/*   Updated: 2024/07/03 21:13:50 by pedromar                                 */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <stdio.h>//
 
 # include "ft_vector.h"
+# include "libft.h"
 # include "wrappers.h"
 
 # ifndef M_PI
@@ -33,6 +34,8 @@
 # define IM1_SX 42
 # define IM1_SY 42
 
+# include "scene.h"
+
 typedef struct s_canvas
 {
 	void	*mlx;
@@ -44,11 +47,26 @@ typedef struct s_canvas
 	char	*data;
 }	t_canvas;
 
+typedef struct s_render
+{
+	t_canvas	*canvas;
+	t_scene		*scene;
+}	t_render;
+
+t_scene		*parser(int argc, char **argv);
 void		*mallox(size_t size);
-int			render_loop(void *r);
-int			key_manager(int key, t_canvas *canvas);
-void		set_control(t_canvas *canvas);
+void		ft_error(char *msg);
+int			render_loop(t_render *r);
+int			key_manager(int key, t_render *render);
+void		set_control(t_render *render);
 t_canvas	*new_canvas(void);
-void		delete_canvas(t_canvas *canvas);
+int			delete_canvas(void *input);
+
+int			set_rgba(t_vec4 c);
+t_vec4		get_rgba(float color);
+int			valid_color(t_vec4 c);
+
+void		ft_iterarr(void **arr, void (*f)(void *));
+int			ft_lenarr(void **arr);
 
 #endif // MINIRT_H
--- src/main.c
+++ src/main.c
@@ -6,21 +6,21 @@
 /*   By: pedromar <pedromar@student.42madrid.com                              */
 /*                                                                            */
 /*   Created: 2024/05/13 17:47:54 by pedromar                                 */
-/*   Updated: 2024/07/03 13:59:55 by pedromar                                 */
+/*   Updated: 2024/07/03 21:16:28 by pedromar                                 */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+#include <string.h>
 
 int	main(int argc, char *argv[])
 {
-	t_canvas	*canvas;
+	t_render	render;
 
-	(void)argc;
-	(void)argv;
 	printf("miniRT %s %s\n", __DATE__, __TIME__);
-	canvas = new_canvas();
-	set_control(canvas);
+	render.scene = parser(argc, argv);
+	render.canvas = new_canvas();
+	set_control(&render);
 	mlx_loop(ft_getmlx());
-	return (EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
