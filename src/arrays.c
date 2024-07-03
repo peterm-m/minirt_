08a0480ab28b89b7ff89aff3ef7f1855aabc9140
pedromar <pedromar@student.42madrid.com>
2024-07-03 21:20:32 +0200

    add arrays

---
+++ src/arrays.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                                            */
+/*   arrays.c                                                                 */
+/*                                                                            */
+/*   By: pedromar <pedromar@student.42madrid.com                              */
+/*                                                                            */
+/*   Created: 2024/07/03 19:22:39 by pedromar                                 */
+/*   Updated: 2024/07/03 21:17:11 by pedromar                                 */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_lenarr(void **arr)
+{
+	int	i;
+
+	i = -1;
+	while (arr[++i])
+		;
+	return (i);
+}
+
+void	ft_iterarr(void **arr, void (*f)(void *))
+{
+	int	i;
+
+	i = -1;
+	while (arr[++i])
+		f(arr[i]);
+}
