075ba9986438102e1957b5b7739774b3c4a26fbf
pedromar <pedromar@student.42madrid.com>
2024-07-07 16:22:54 +0200

    fix bug in colors

--- utils/colors.c
+++ utils/colors.c
@@ -7,20 +7,21 @@
 
 int	set_rgba(t_vec4 c)
 {
-	return (((int)(MAX_RGB * c.b)) \
-		+ ((int)(MAX_RGB * c.g) << 8) \
-		+ ((int)(MAX_RGB * c.r) << 16) \
-		+ ((int)(MAX_RGB * c.a) << 24));
+	return (*(int *)(unsigned char [4]){\
+		(unsigned char)(MAX_RGB * c.b), \
+		(unsigned char)(MAX_RGB * c.g), \
+		(unsigned char)(MAX_RGB * c.r), \
+		(unsigned char)(MAX_RGB * c.a)});
 }
 
-t_vec4	get_rgba(float color)
+t_vec4	get_rgba(int color)
 {
 	t_vec4	c;
 
-	c.r = (((unsigned int)color >> 16) & 0xFF);
-	c.g = (((unsigned int)color >> 8) & 0xFF);
-	c.b = (((unsigned int)color) & 0xFF);
-	c.a = 0.0f;
+	c.a = (float)(((unsigned char *)&color)[3]);
+	c.r = (float)(((unsigned char *)&color)[2]);
+	c.g = (float)(((unsigned char *)&color)[1]);
+	c.b = (float)(((unsigned char *)&color)[0]);
 	return (c);
 }
 
--- utils/in_range.c
+++ utils/in_range.c
@@ -6,4 +6,3 @@ int	in_range(float f, float upper_lim, float lower_lim)
 	return (isfinite(f) && (isgreaterequal(f, lower_lim)
 			&& islessequal(f, upper_lim)));
 }
-
