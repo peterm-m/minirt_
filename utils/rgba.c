63f973a487236d99af009b66353dbad427f00912
pedromar <pedromar@student.42madrid.com>
2024-07-08 14:53:45 +0200

    fix rgba_sum error

--- utils/rgba.c
+++ utils/rgba.c
@@ -1,14 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   rgba_tools.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: pedromar <pedromar@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/07 22:27:38 by pedromar          #+#    #+#             */
-/*   Updated: 2024/07/07 22:58:47 by pedromar         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "utils.h"
 
@@ -36,8 +25,8 @@ t_vec4	rgba_brightness(t_vec4 rgba, float brightness)
 t_vec4	rgba_sum(t_vec4 rgba1, t_vec4 rgba2)
 {
 	return (ft_vec4(
-			MIN(rgba1.r + rgba1.r, 1.0f),
-			MIN(rgba1.g + rgba1.g, 1.0f),
-			MIN(rgba1.b + rgba1.b, 1.0f),
-			MIN(rgba1.a + rgba1.a, 1.0f)));
+			MIN(rgba1.x + rgba2.x, 1.0f),
+			MIN(rgba1.y + rgba2.y, 1.0f),
+			MIN(rgba1.z + rgba2.z, 1.0f),
+			MIN(rgba1.w + rgba2.w, 1.0f)));
 }
