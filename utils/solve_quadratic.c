a8e2c126dd83375d0b2097a49cf3c11084ab1a55
pedromar <pedromar@student.42madrid.com>
2024-07-20 12:19:51 +0200

    norma utils

--- utils/solve_quadratic.c
+++ utils/solve_quadratic.c
@@ -6,7 +6,7 @@
 /*   By: pedromar <pedromar@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 13:44:15 by pedromar          #+#    #+#             */
-/*   Updated: 2024/07/15 14:05:24 by pedromar         ###   ########.fr       */
+/*   Updated: 2024/07/20 12:02:02 by pedromar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ float	solve_quadratic(float a, float b, float c, float *roots)
 	float	discriminat;
 	float	q;
 
-	discriminat = b * b - 4.0f * a * c;
+	discriminat = b * b -4.0f * a * c;
 	if (isless(discriminat, 0.0f))
 	{
 		roots[0] = INFINITY;
@@ -25,12 +25,12 @@ float	solve_quadratic(float a, float b, float c, float *roots)
 	}
 	else if (isgreater(discriminat, 0.0f))
 	{
-		q = - 0.5f * (b + (1.0f - 2.0f * isgreater(b, 0.0f)) * sqrt(discriminat));
+		q = -0.5 * (b + (1.0 -2.0 * isgreater(b, 0.0)) * sqrt(discriminat));
 		roots[0] = q / a;
 		roots[1] = c / q;
 	}
 	else
-		roots[0] = - 0.5f * b / a;
+		roots[0] = -0.5f * b / a;
 	if (isgreater(roots[0], roots[1]))
 	{
 		q = roots[0];
---
+++ utils/xpm_file.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   xpm_file.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pedromar <pedromar@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/20 11:45:20 by pedromar          #+#    #+#             */
+/*   Updated: 2024/07/20 12:03:44 by pedromar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils.h"
+
+t_xpm	*ft_xpm_image(char *file_name)
+{
+	int		len_file_name;
+	t_xpm	*xpm;
+
+	len_file_name = ft_strlen(file_name);
+	if (len_file_name < 4 && \
+		ft_strncmp(file_name + len_file_name - 4, ".xpm", 4))
+	{
+		printf("Invalid name: mandatory extension xpm");
+	}
+	xpm = mallox(sizeof(t_xpm));
+	xpm->im = mlx_xpm_file_to_image(ft_getmlx(), file_name, \
+		&xpm->h, &xpm->w);
+	if (!xpm->im)
+	{
+		printf("xpm file error\n");
+		return (NULL);
+	}
+	xpm->data = mlx_get_data_addr(xpm->im, &xpm->bpp, &xpm->sl, &xpm->endian);
+	if (!xpm->data)
+	{
+		printf("xpm file error\n");
+		mlx_destroy_image(ft_getmlx(), xpm->im);
+		return (NULL);
+	}
+	return (xpm);
+}
+
+t_vec4	get_pixel_xpm(t_xpm *xpm, t_vec2 *pix)
+{
+	char	*dst;
+	t_vec4	color;
+	t_ivec2	ij;
+
+	ij.x = (int)floorf(xpm->h * pix->x);
+	ij.y = (int)floorf(xpm->w * pix->y);
+	dst = (xpm->data + (ij.y * xpm->sl + ij.x * (xpm->bpp / 8)));
+	color = int_to_rgba(*(unsigned int *)dst);
+	return (color);
+}
