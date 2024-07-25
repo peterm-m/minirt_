8d0042f28d83c7c8f25ed3bd0c0580e009afd685
pedromar <pedromar@student.42madrid.com>
2024-07-25 19:28:06 +0200

    WIP

--- src/render_loop.c
+++ src/render_loop.c
@@ -23,7 +23,7 @@ int	shading(t_scene *scene, t_hit *h)
 	surface_info(h);
 	light_color = scene->a->color; // mas componente emisiva del cuerpo
 	while (scene->l[++i])
-		if (check_shadow(scene, h, i) == NULL)
+		if (check_shadow(scene, h, i))
 			phong_term(scene->l[i], h, &light_color);
 	return (rgba_to_int(ft_mulv4v(h->o->color, light_color)));
 }
