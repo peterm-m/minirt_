6eccbfb42dd83498c2273567545e41de13be9413
pedromar <pedromar@student.42madrid.com>
2024-07-31 19:43:55 +0200

    fix camera trasformation

--- objects/parser_material.c
+++ objects/parser_material.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser_material.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adiaz-uf <adiaz-uf@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pedromar <pedromar@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 14:53:47 by pedromar          #+#    #+#             */
-/*   Updated: 2024/07/30 18:49:49 by adiaz-uf         ###   ########.fr       */
+/*   Updated: 2024/07/31 17:24:58 by pedromar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	fill_material(t_vec4 *rgba, t_material *mat)
 {
 	mat->mat_info = 0;
 	mat->k_a = ft_divv4f(*rgba, 255.0f);
-	mat->k_d = mat->k_a;
-	mat->k_s = mat->k_a;
+	mat->k_d = ft_divv4f(*rgba, 255.0f);
+	mat->k_s = ft_divv4f(*rgba, 255.0f);
 	mat->glossiness = 100.0f;
 	mat->texture = NULL;
 	mat->bump_map = NULL;
--- src/shaders.c
+++ src/shaders.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   shaders.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adiaz-uf <adiaz-uf@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pedromar <pedromar@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 23:32:29 by pedromar          #+#    #+#             */
-/*   Updated: 2024/07/30 19:42:19 by adiaz-uf         ###   ########.fr       */
+/*   Updated: 2024/07/31 16:49:09 by pedromar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	phong_term(t_light *l, t_hit *h, t_vec4 *color)
 	spec_factor = -2.0f * diff_factor * ft_dotv3(h->primary.d, h->normal)
 		+ ft_dotv3(h->primary.d, h->secundary.d);
 	spec_factor = pow(MAX(spec_factor, 0.0f), h->o->material.glossiness);
-	geometric_factor = 1.0f / (h->secundary.t * h->secundary.t);
+	geometric_factor = 1.0f;
 	rgba_sum(color, ft_mulv4v(l->color, h->data_o[DIFFUSE]), \
 		diff_factor * geometric_factor);
 	rgba_sum(color, ft_mulv4v(l->color, h->data_o[SPECULAR]), \
